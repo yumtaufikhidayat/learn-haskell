e50836abd076757d7b61c88f2c41fb782703c638
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-30 05:53:51 +0700

    Implement some functions with many parameters.

---
+++ myParamFunction.hs
@@ -0,0 +1,11 @@
+--f(x,y)=3x+5y+2
+f :: Int -> Int -> Int
+f x y = 3*x + 5*y + 2
+
+-- g(x,y)=x2+y2
+g :: Int -> Int -> Int
+g a b = a^2 + b^2
+
+-- h(x,y)=16−(x−3)2−(y−2)2
+h :: Int -> Int -> Int
+h c d = 16-((c-3)^2)-((d-2)^2)
